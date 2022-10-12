ab807bcdc28c4c6db1be4b660f4edd57e8fba212
UTS23 <brayanstevenmoreno@uts.edu.co>
2022-10-12 11:00:38 -0500

    subiendo programas de las clases

---
+++ clase_del_viernes/ejercicio2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void insertiont_short(int a[], int n);
+for(int i =i;i<n: i++);
+int key = a[i];
+int j=i-1;
+while(key<a[j]&&j>=0){
+	a[j+1]=a[j];
+	--j;
+}
+a[j+1]=key;
+}
+
+
+void pantalla ( int a [ ] , int n )
+for ( int j = 0 ; j < n - 1 ; j ++ )
+      printf ( " % d . , a [ j ] ) ;
+}
+printf ( " % d \ n " , a [ n - 1 ] ) ;
