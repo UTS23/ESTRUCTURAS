82e686387b317d308eb8580c581e55c600fecc81
UTS23 <brayanstevenmoreno@uts.edu.co>
2022-10-23 10:54:23 -0500

    .

---
+++ clase_del_viernes/ejer.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main() {
+
+
+	float a,b,c,d   ,respuesta=0;
+
+	cout<<"el valor a: ";cin>>a;
+	cout<<"el  valor b: ";cin>>b;
+
+cout<<"el  valor c: ";cin>>c;
+cout<<"el  valor d: ";cin>>d;
+respuesta =(a+b) / (c+d);
+cout.precision(3);
+cout<<"\nE1 la respuesta es : "<<respuesta<<endl;
+return 0;
+
+
+
+}
